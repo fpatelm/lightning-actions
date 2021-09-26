655080cc799c7ff3dbbf9db52955bdbcaaa9a4f5
Faizal Patel <fpatelm@users.noreply.github.com>
2021-09-26 13:09:10 +0100

    mock other lwc components

---
+++ force-app/main/default/lwc/otherComponents/otherComponents.js
@@ -0,0 +1,4 @@
+import { LightningElement } from 'lwc';
+//automatically creat test package
+//sfdx force:lightning:lwc:test:create -f force-app/main/default/lwc/otherComponents/otherComponents.js
+export default class OtherComponents extends LightningElement {}
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
-const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
-    ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+  ...jestConfig,
+  modulePathIgnorePatterns: ["<rootDir>/.localdevserver"],
+
+  
+  //add any custom configurations here
+  moduleNameMapper: {
+    '^c/displayPanel$': '<rootDir>/force-app/test/jest-mocks/c/displayPanel',
+    "^lightning/button$": "<rootDir>/force-app/test/jest-mocks/lightning/button"
+  }
 };
